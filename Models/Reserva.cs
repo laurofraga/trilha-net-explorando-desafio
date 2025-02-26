5d35971c921fa9e14f4a0dd34806bf182ca614fc
laurofraga <fragalauro12@gmail.com>
2025-02-26 10:18:08 -0300

    resolucao do desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,8 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                Console.WriteLine("Capacidade insuficiente para a quantidade de hóspedes.");
+
             }
         }
 
@@ -37,7 +39,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public decimal CalcularValorDiaria()
@@ -49,11 +58,14 @@ namespace DesafioProjetoHospedagem.Models
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = DiasReservados * Suite.ValorDiaria * 0.9m;
             }
-
+            else
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+            }
             return valor;
         }
     }
